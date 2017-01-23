a334bc8cb9db9973050b68d5e231d7eb3aee6907
jasonrez <jasonrez@hotmail.co.uk>
2017-01-23 22:30:38 +0000

    wireup controls conponent

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -2,25 +2,32 @@ import React from 'react';
 
 export default class Controls extends React.Component {
 
+  onStatusChange(newStatus){
+    return () => {
+      this.props.onStatusChange(newStatus);
+    }
+  }
+
   render(){
     let {countdownStatus} = this.props;
     let renderStartStopButton = () => {
       if (countdownStatus === 'started') {
-        return <button className="button secondary">Pause</button>
+        return <button className="button secondary" onClick={this.onStatusChange('paused').bind(this)}>Pause</button>
       } else if (countdownStatus === 'paused') {
-        return <button className="button primary">Start</button>
+        return <button className="button primary" onClick={this.onStatusChange('started').bind(this)}>Start</button>
       }
     };
 
     return (
       <div className="controls">
-        <button className="button alert hollow">Clear</button>
         {renderStartStopButton()}
+        <button className="button alert hollow" onClick={this.onStatusChange('stopped').bind(this)}>Clear</button>
       </div>
     )
   };
 };
 
 Controls.propTypes = {
-  countdownStatus : React.PropTypes.string.isRequired
+  countdownStatus : React.PropTypes.string.isRequired,
+  onStatusChange : React.PropTypes.func.isRequired
 };
--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Clock from 'Clock';
 import CountdownForm from 'CountdownForm';
+import Controls from 'Controls';
 
 
 export default class Countdown extends React.Component{
@@ -18,6 +19,12 @@ export default class Countdown extends React.Component{
       switch (this.state.countdownStatus) {
         case 'started':
           this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer)
+          this.timer = undefined;
           break;
       }
     }
@@ -38,13 +45,23 @@ export default class Countdown extends React.Component{
       });
   };
 
+  handleStatusChange(newStatus) {
+    this.setState({countdownStatus: newStatus});
+  }
+
   render(){
-    let {count} = this.state;
+    let {count, countdownStatus} = this.state;
+    let renderControlArea = () => {
+      if(countdownStatus !== 'stopped'){
+        return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange.bind(this)}/>
+      } else {
+        return <CountdownForm onSetCountdown={this.handleSetCountdown.bind(this)}/>;
+      }
+    }
     return (
       <div>
         <Clock totalSeconds={count}/>
-        <CountdownForm onSetCountdown={this.handleSetCountdown.bind(this)}/>
-
+        {renderControlArea()}
       </div>
     );
   };
